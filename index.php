1e98e51516c163a3639571e833958e2b2fec699e
Manuel Lemaire <2012manuell@psi.kiev.ua>
2013-06-09 21:21:09 +0200

    change in plan, using a jquery based API now

--- index.php
+++ index.php
@@ -4,20 +4,15 @@
 	mysql_set_charset('utf8',$BDD);
 	mysql_select_db('test');
 
-	$sql = "SELECT * FROM information";
+	$sql = "SELECT * FROM interview";
 	$sql = mysql_query($sql);
 
 	$interview = array();
 	while($data=mysql_fetch_array($sql)) {	
 		$interview[] = array(
-			"id" => $data['id'],
-			"nom" => iconv("UTF-8", "ISO-8859-1//TRANSLIT", $data['nom']),
-			"prenom" => iconv("UTF-8", "ISO-8859-1//TRANSLIT", $data['prenom']), // lors des caractère spéciaux
-			"bio" => iconv("UTF-8", "ISO-8859-1//TRANSLIT", $data['bio']),
-			"wiki" => $data['wiki'],
-			"facebook" => $data['facebook'],
-			"twitter" => $data['twitter'],
-			"site" => $data['site'],
+			"nom" => $data['nom']),
+			"prenom" => $data['prenom']), // lors des caractère spéciaux
+			"bio" => $data['bio']),
 			"img" => $data['image']
 		);
 	}
@@ -28,6 +23,7 @@
 		<meta charset="utf-8">
 		<title>test</title>
 		<link rel="stylesheet" href="css/normalize.css"/>
+		<link rel="stylesheet" href="css/sc-player-minimal.css" type="text/css">
 		<link rel="stylesheet" href="css/style.css"/>
 		<?php
 			$interview = json_encode($interview);
@@ -35,22 +31,17 @@
 				var interview = ".$interview.";
 		</script>";
 		?>
-		<script type="text/javascript" src="scripts/soundcloud.player.api.js"></script>
-		<script type="text/javascript">
-		   soundcloud.addEventListener('onPlayerReady', function(player, data) {
-
-   			});
-		</script>
+		<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+		<script type="text/javascript" src="js/soundcloud.player.api.js"></script>
+		<script type="text/javascript" src="js/sc-player.js"></script>
+	</head>
+	<body>
 		<div id="haut">
 			<div id="logo">
 
 			</div>
 			<div id="lecteur">
-				<object height="81" width="100%" id="myPlayer" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">
-  					<param name="movie" value="http://player.soundcloud.com/player.swf?url=https://soundcloud.com/matas/hobnotropic&enable_api=true&object_id=myPlayer"></param>
- 					<param name="allowscriptaccess" value="always"></param>
- 					<embed allowscriptaccess="always" height="81" src="http://player.soundcloud.com/player.swf?url=https://soundcloud.com/matas/hobnotropic&enable_api=true&object_id=myPlayer" type="application/x-shockwave-flash" width="100%" name="myPlayer"></embed>
-				</object>
+				<a href="https://soundcloud.com/macklemore/macklemore-x-ryan-lewis-cant" class="sc-player"></a>
 			</div>
 			<div id="info">
 
@@ -60,12 +51,20 @@
 			
 		</ul>
 		<div id="footer">
-			
+
 		</div>
-		<script >
-				for (var l = 0; l < 30; l++) {
-					document.getElementById('liste').innerHTML += "<li id='number_"+ (l+1) +"' onclick='details(this)'></li>";
-				};
+		<script>
+			$(function() {
+				$(document).ready(function(){
+					for (var i = 0; i < interview.length; i++) {
+						$('#liste').append("<li data-index='"+ i +"'></li>");
+					};
+					$('#liste li').click(function(){
+						var index = $(this).attr('data-index');
+						$('#info').html("<p id='ligne_nom'>hi there " + interview[index].prenom + "</p>");
+					});
+				});
+			});
 		</script>
 	</body>
 </html>
